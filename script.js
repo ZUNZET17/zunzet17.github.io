30c58572750b28d7abd729506052761a77221e54
Chris Gomez <77113638+ZUNZET17@users.noreply.github.com>
2024-02-16 12:29:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
   document.addEventListener('DOMContentLoaded', () => {
     const textInput = document.querySelector('.js-custom-txt');
     const imgInput = document.querySelector('.js-custom-img')
-    const button = document.querySelector('.js-custom-fields-btn')
+    const button = document.createElement('button');
+    button.textContent = 'Clear fields.';
+    document.querySelector('.js-custom-fields').appendChild(button);
+    
 
     textInput.addEventListener('input', (event) => {
       if ( textInput.value ) {
